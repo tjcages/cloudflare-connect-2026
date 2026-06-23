8780ef9eb6b3d87d9d0b9eb2fc8c06f359032d8d
necatikcl <necatifk07@gmail.com>
2026-06-23 10:38:21 +0300

    fix(lab): full-viewport canvas, remove body scrollbar (global reset)

--- apps/lab/src/main.tsx
+++ apps/lab/src/main.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { LabApp } from "./LabApp";
+import "./index.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
